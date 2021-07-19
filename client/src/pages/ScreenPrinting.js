4fe1a3b30d5d5a5136b767899d70224b285fefab
jon <jonlyeatman@gmail.com>
2021-07-19 00:11:07 -0400

    Screen printing page classes

--- client/src/pages/ScreenPrinting.js
+++ client/src/pages/ScreenPrinting.js
@@ -3,31 +3,36 @@ import React from 'react'
 function ScreenPrinting() {
     return (
         <div className="screenprinting-container">
-            <div>
-                <p>
+            <div className="screen-primary-text-container">
+                <p className="screen-primary-text">
                     Screen printing is the most common way to decorate custom apparel.
+                    <br />
                     It is perfect for larger orders 
+                    <br />
                     If you want your design on 25 or 500 shirts this is going to be the best option for you
                 </p>
             </div>
 
-            <div>
+            <div className="screen-img-container">
                 <img 
                 src="" 
                 alt="screen printing" 
+                className="screen-img"
                 />
                 <img 
                 src="" 
                 alt="screen printing" 
+                className="screen-img"
                 />
                 <img 
                 src="" 
                 alt="screen printing" 
+                className="screen-img"
                 />
             </div>
 
-            <div>
-                <p>
+            <div className="screen-secondary-text-container">
+                <p className="screen-secondary-text"> 
                 For best results we commonly use what is referred to in the industry as a 
                 spot color process of printing. This means that each color is separated onto an 
                 individual screen before printing. Through this process it is very common for 
