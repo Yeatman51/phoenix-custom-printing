de95eb13ef4754a5b3acd5bc79ccddefd64cf804
jon <jonlyeatman@gmail.com>
2021-07-21 22:11:14 -0400

    Homepage set up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,46 @@
 import React from "react";
+import HomeCard from "../components/HomeCard"
 
 
 
 
 function Home() {
+
+  const homeCardInfo = [
+    {
+      key: 1,
+      imgSrc:"/",
+      alt:"pic",
+      title:"afk",
+      description:"hjl ksd fhuoi wef"
+    },
+  ]
+
   return (
     <div className="home-container">
         
         
-        <div className="">
-            <p>
-                
-            </p>
-        </div>
+      <div className="home-title-text">
+          <p>
+          With our production facility and design office located in Milford Michigan we’ve been proudly serving the greater Detroit area for years.
+          <br /><br />
+          However with our shipping connections and distribution skills we are able to provide a superior product and service to both people and companies across the country
+          <br /><br />
+          No matter the state or the Time Zone that you call home we will gladly provide superior quality care and apparel to you
+          </p>
+      </div>
+    
       
+        {homeCardInfo.map(card => (
+          <HomeCard 
+            key={card.key}
+            title={card.title}
+            imgSrc={card.imgSrc}
+            alt={card.alt}
+            description={card.description}
+          /> 
+        ))}
+     
 
     </div>
   
