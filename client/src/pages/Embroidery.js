4a4f0972e3c7d958e4f28e5535b71ea7604732ba
jon <jonlyeatman@gmail.com>
2021-08-02 21:44:28 -0400

    EMB set up

--- client/src/pages/Embroidery.js
+++ client/src/pages/Embroidery.js
@@ -3,7 +3,33 @@ import React from 'react'
 function Embroidery() {
     return (
         <div className="embroidery-container">
-            EMB
+            <div className="embroidery-primary-text-container">
+                <p className="embroidery-primary-text">
+                    When you need a high-end look and finish we take the time to make sure that every single stitch and thread pattern has been maximized for your design and artwork
+                    <br />
+                    Hats, Polos, Quarter Zip Jackets, Business Apparel, Professional Jerseys
+                </p>
+            </div>
+
+            <div className="embroidery-img-container">
+                <img 
+                src="" 
+                alt="embroidery machine" 
+                className="emb-img"
+                />
+
+                <img 
+                src="" 
+                alt="embroidery machine" 
+                className="emb-img"
+                />
+
+                <img 
+                src="" 
+                alt="embroidery machine" 
+                className="emb-img"
+                />
+            </div>
         </div>
     )
 }
